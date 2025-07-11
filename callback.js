b9748ef275d44abda1cd780a6107a19c3774eea7
krishnajha23 <krishjhaxd@gmail.com>
2025-07-10 22:55:39 -0400

    lol

--- callback.js
+++ callback.js
@@ -8,7 +8,6 @@ if (code) {
   })
     .then(res => res.json())
     .then(data => {
-      console.log("gotcha")
       document.getElementById('output').textContent = JSON.stringify(data, null, 2);
     })
     .catch(err => {

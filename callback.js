1983f546d0b6b0aac2dc1fc1786c716301721fd5
krishnajha23 <krishjhaxd@gmail.com>
2025-07-11 01:10:10 -0400

    lol

--- callback.js
+++ callback.js
@@ -9,7 +9,25 @@ if (code) {
     .then(res => res.json())
     .then(data => {
       document.getElementById('output').textContent = JSON.stringify(data, null, 2);
-      console.log(data.access_token);
+      const token = data.access_token;
+      const openid = data.open_id;
+      fetch('https://open.tiktokapis.com/v2/video/list/?fields=cover_image_url,id,title', {
+        method: 'POST',
+        headers: {
+          'Authorization': token,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          max_count: 20
+        })
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log("Video list response:", data);
+        })
+        .catch(err => {
+          console.error("Error fetching video list:", err);
+        });
     })
     .catch(err => {
       console.log("CODE: " + code);

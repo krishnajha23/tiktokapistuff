78fffce6d6ff8694646213df6258711b1ac27cf5
krishnajha23 <krishjhaxd@gmail.com>
2025-07-10 20:59:03 -0400

    superlol

--- login.js
+++ login.js
@@ -1,9 +1,14 @@
-const client_key = 'sbawrdi2z8wa4uuuwg';
-const redirect_uri = encodeURIComponent('https://krishnajha23.github.io/tiktokapistuff/callback.html');
-const scope = 'video.list';
-const state = 'abc123';
-
 document.getElementById('tiktok-login-btn').onclick = function () {
-  const authUrl = `https://www.tiktok.com/v2/auth/authorize/?client_key=${client_key}&scope=${scope}&response_type=code&redirect_uri=${redirect_uri}&state=${state}`;
-  window.location.href = authUrl;
+  fetch('https://tiktokapibackend.onrender.com/get-login-url')
+    .then(res => res.json())
+    .then(data => {
+      if (data.url) {
+        window.location.href = data.url;
+      } else {
+        alert('Failed to get TikTok login URL');
+      }
+    })
+    .catch(err => {
+      console.error('Error getting login URL:', err);
+    });
 };

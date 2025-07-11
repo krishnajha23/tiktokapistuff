1779e0b6f267be7f544216b2dc5c0b9f13e11c7a
krishnajha23 <krishjhaxd@gmail.com>
2025-07-10 23:24:18 -0400

    lol

--- callback.js
+++ callback.js
@@ -1,6 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 const code = params.get('code');
 if (code) {
+  console.log("CODE: " + code);
   fetch('https://tiktokapibackend.onrender.com/get-access-token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -8,7 +9,6 @@ if (code) {
   })
     .then(res => res.json())
     .then(data => {
-      console.log("CODE: " + code);
       document.getElementById('output').textContent = JSON.stringify(data, null, 2);
     })
     .catch(err => {

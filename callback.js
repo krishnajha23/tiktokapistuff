5fa0514785f618f609fb05b6e5931f4a6521ca52
krishnajha23 <krishjhaxd@gmail.com>
2025-07-10 23:59:21 -0400

    lol

--- callback.js
+++ callback.js
@@ -4,7 +4,7 @@ if (code) {
   fetch('https://tiktokapibackend.onrender.com/get-access-token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: code
+    body: JSON.stringify({ code })
   })
     .then(res => res.json())
     .then(data => {
